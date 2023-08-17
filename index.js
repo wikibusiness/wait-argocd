2359a7542d9b80f691afdd9a74354317e09ee054
Theo Mathieu <tmathieu.github@fastmail.com>
2023-08-17 12:05:26 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const axios = require('axios')
+
+const waitForSomeMs = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+};
+
+  
+const run = async () => {
+    try {
+        // `who-to-greet` input defined in action metadata file
+        const ARGO_PASSWORD = core.getInput('argo_password');
+        const APPLICATION_NAME = core.getInput('application_name');
+        const MAX_RETRIES = parseInt(core.getInput('max_retries'));
+        const WAIT_MS = parseInt(core.getInput('wait_ms'));
+
+
+        const { data: tokenData } = await axios.post(
+            'https://argo.anchor.io/api/v1/session',
+            { username: 'admin', password: ARGO_PASSWORD }
+        );
+
+        let index = 0;
+        let withRefresh = '?refresh=true'
+        while (index < MAX_RETRIES) {
+            console.log(`Try ${index}`)
+            const { data: dataApplication } = await axios.get(
+                `https://argo.anchor.io/api/v1/applications/${APPLICATION_NAME}${withRefresh}`,
+                { headers: { Authorization: `Bearer ${tokenData.token}` } }
+            );
+
+            if (dataApplication.status.health.status === 'Healthy') {
+                console.log(`Application ${APPLICATION_NAME} is healthy.`)
+                break;
+            }
+
+            console.log(`Application ${APPLICATION_NAME} is not healthy. Status is ${dataApplication.status.health.status}`)
+            withRefresh = ''
+            index++;
+            waitForSomeMs(WAIT_MS)
+        }
+
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+run();
